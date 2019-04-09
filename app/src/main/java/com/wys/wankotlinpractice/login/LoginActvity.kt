824adc18dca2c3ac447a79b3cc41aca1846f8515
WangYongSheng <m18301357350@163.com>
2019-04-09 10:17:51 +0800

    fixed

---
+++ app/src/main/java/com/wys/wankotlinpractice/login/LoginActvity.kt
@@ -0,0 +1,13 @@
+package com.wys.wankotlinpractice.login
+
+import android.os.Bundle
+import com.wys.wankotlinpractice.R
+import com.wys.wankotlinpractice.base.BaseActivity
+
+class LoginActvity : BaseActivity() {
+    override fun contentLayoutId(): Int = R.layout.activity_login
+
+    override fun init(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
